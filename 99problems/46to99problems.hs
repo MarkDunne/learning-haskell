9059e22c5edd9bb0d10156822e1338878c4ad507
Mark Dunne <mark.dunne02@gmail.com>
2014-02-19 02:16:31 +0000

    huffman encoder

--- 99problems/46to99problems.hs
+++ 99problems/46to99problems.hs
@@ -1,3 +1,5 @@
+import Data.Ord
+import Data.List
 import Control.Monad
 
 --Problem 46 Part 1
@@ -16,12 +18,12 @@ table :: (Bool -> Bool -> Bool) -> IO ()
 table f = mapM_ print output
     where output = [[a, b, (f a b)] | a <- [True, False], b <- [True, False]]
 
---Problem 47
+--Problem 48
 tablen :: Int -> ([Bool] -> Bool) -> IO ()
 tablen n f = mapM_ print output
     where output = [inputs ++ [f inputs] | inputs <- replicateM n [True, False]]
 
---Problem 48
+--Problem 49
 binaryToGray :: [Int] -> [Int]
 binaryToGray num = xor shifted num
     where shifted = 0 : init num
@@ -30,4 +32,24 @@ binaryToGray num = xor shifted num
 gray :: Int -> [[Int]]
 gray n = map binaryToGray (replicateM n [0, 1])
 
-main = print $ gray 3
+--Problem 50
+--huffman :: [(Char, Int)] -> [(Char, String)]
+
+data HTree a = Leaf a | Branch (HTree a) (HTree a)
+	deriving (Show)
+
+hstep :: [(Int, HTree Char)] -> [(Int, HTree Char)]
+hstep ls = combineSmallest $ sortBy (comparing fst) ls
+	where combineSmallest ((w1,l1):(w2,l2):ls) = [(w1 + w2, Branch l1 l2)] ++ ls
+
+serialize :: HTree Char -> [(Char, String)]
+serialize (Branch left right) = 
+	[(x, '0':prefix) | (x, prefix) <- serialize left] ++
+	[(x, '1':prefix) | (x, prefix) <- serialize right]
+serialize (Leaf x) = [(x, "")]
+
+huffEncode :: [(Char, Int)] -> [(Char, String)]
+huffEncode input = serialize . snd . head $ until (\ls -> length ls == 1) hstep (setupData input)
+	where setupData freqs = [ (w, Leaf e) | (e, w) <- sortBy (comparing snd) freqs]
+
+main = print $ "hello"
