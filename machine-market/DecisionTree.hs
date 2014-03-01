c0bd3197b765113ffe6a2717903ec45189ed2b82
Mark Dunne <mark.dunne02@gmail.com>
2014-03-01 01:06:57 +0000

    figuring out better data structure

--- machine-market/DecisionTree.hs
+++ machine-market/DecisionTree.hs
@@ -1,26 +1,43 @@
 module DecisionTree where
 
-	import Data.List
-	import Debug.Trace
+import Data.List
+import Debug.Trace
 
-	entropy :: [Bool] -> Double
-	entropy outcomes = calc probTrue + calc (1 - probTrue)
-		where probTrue = genericLength (filter id outcomes) / genericLength outcomes
-			  calc 1 = 0
-			  calc 0 = 0
-			  calc prob = -prob * logBase 2 prob
+data Column a = Column [a]
+  deriving (Show)
 
-	tableEntropy :: ([Bool], [Bool]) -> Double
-	tableEntropy (c1, c2) = calc c1 + calc c2
-		where coeff c = genericLength c / (genericLength c1 + genericLength c2)
-			  calc c = coeff c * entropy c
-			  
-	infoGain :: [Bool] -> [Bool] -> Double
-	infoGain inputs outcomes = entropy outcomes - tableEntropy table
-		where table = foldl makeTable ([], []) (zip inputs outcomes)
-			  makeTable (trueCol, falseCol) (input, outcome)
-				  | outcome = (input:trueCol, falseCol)
-				  | otherwise = (trueCol, input: falseCol)
-			  
-	main = do
-		print $ infoGain [True, False, True] [True, False, True]
+data Table a = Table {
+  attributes :: [Column a],
+  outcomes :: Column Bool
+} deriving (Show)
+
+entropy :: Table a -> Double
+entropy (Table _ (Column outcomes)) = calc probTrue + calc (1 - probTrue)
+    where probTrue = genericLength (filter id outcomes) / genericLength outcomes
+          calc 0 = 0
+          calc 1 = 0
+          calc prob = -prob * logBase 2 prob
+
+--splitColumn :: Table -> Int -> (Table, Table)
+--splitColumn (Table cols (BoolColumn) = (a, a)
+
+--entropy (StrColumn event) = map entropy (splitColumn)
+
+--tableEntropy :: ([Bool], [Bool]) -> Double
+--tableEntropy (c1, c2) = calc c1 + calc c2
+--    where coeff c = genericLength c / (genericLength c1 + genericLength c2)
+--          calc c = coeff c * entropy c
+          
+--infoGain :: Ord a => [a] -> [Bool] -> Double
+--infoGain inputs outcomes = entropy outcomes - tableEntropy table
+--    where table = foldl makeTable ([], []) (zip inputs outcomes)
+--          makeTable (trueCol, falseCol) (input, outcome)
+--              | outcome = (input:trueCol, falseCol)
+--              | otherwise = (trueCol, input: falseCol)
+          
+main = do
+    print $ Table [Column [1]] (Column [True])
+
+
+
+--infoGain ["sunny","sunny","overcast","rain","rain","rain","overcast","sunny","sunny","rain","sunny","overcast","overcast","rain"] [False, False, True, True, True, False, True, False, True, True, True, True, True, False]
