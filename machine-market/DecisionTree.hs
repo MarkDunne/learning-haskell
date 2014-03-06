ac382890db763975b1866e573dadf606d71be592
Mark Dunne <mark.dunne02@gmail.com>
2014-03-06 01:08:10 +0000

    start of building a tree

--- machine-market/DecisionTree.hs
+++ machine-market/DecisionTree.hs
@@ -12,6 +12,12 @@ data Table a = Table {
     rows :: [Row a]
 } deriving (Show)
 
+data DecisionTree a = DecisionLeaf (Table a) | DecisionBranch {
+  decisionTable :: Table a,
+  subDecisions :: Map.Map a (DecisionTree a),
+  decisionName :: String
+} deriving (Show)
+
 numAttributes :: Table a -> Int
 numAttributes (Table rows) = numAttributesRow $ head rows
   where numAttributesRow (Row attributes outcome) = length attributes 
@@ -21,6 +27,9 @@ average ls = sum ls / genericLength ls
 
 removeNth :: [a] -> Int -> [a]
 removeNth ls n = map (\(x, _) -> x) . filter (\(_, num) -> num /= n) $ zip ls [0..]
+
+addRowToTable :: Table a -> Row a -> Table a
+addRowToTable (Table rows) row = Table (row:rows)
 
 binaryEntropy :: [Bool] -> Double
 binaryEntropy inputs = calc probTrue + calc (1 - probTrue)
@@ -38,13 +47,26 @@ columnEntropy table attributeCol = weightedAverage (Map.elems (splitColumn table
     where weightedAverage columns = (sum $ map weight columns) / (genericLength $ rows table)
           weight column = binaryEntropy column * genericLength column
 
-splitTable :: Ord k => Table k -> Int -> [Table k]
-splitTable (Table rows) colNum = map (\rows -> Table rows) . Map.elems $ foldl f Map.empty rows
+splitTable :: Ord k => Table k -> Int -> Map.Map k (Table k)
+splitTable (Table rows) colNum = Map.map (\rows -> Table rows) $ foldl f Map.empty rows
     where f m (Row attributes outcome) = Map.insertWith (++) (attributes !! colNum) [Row (newAttributes attributes) outcome] m
           newAttributes attributes = removeNth attributes colNum
 
+--buildTree :: Table a -> DecisionTree a
+buildTree :: Ord a => DecisionTree a -> (Int -> String) -> DecisionTree a
+buildTree (DecisionBranch table _ name) namingFunc = DecisionBranch table subDecisions decisionName
+  where nextDecision = minimumBy (comparing $ columnEntropy table) [0.. numAttributes table - 1]
+        subDecisions = Map.map (\table -> DecisionLeaf table) $ splitTable table nextDecision
+        decisionName = namingFunc nextDecision
+
 main = do
-  print $ splitTable testData 0
+  print $ buildTree (DecisionBranch testData Map.empty "start") columnNameFuncTest
+
+columnNameFuncTest :: Int -> String
+columnNameFuncTest 0 = "Outlook"
+columnNameFuncTest 1 = "Temperature"
+columnNameFuncTest 2 = "Humidity"
+columnNameFuncTest 3 = "Windy"
 
 testData = Table [Row ["sunny", "hot", "high", "false"] False,
                   Row ["sunny", "hot", "high", "true"] False,
